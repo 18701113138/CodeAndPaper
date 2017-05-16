027da39772854baa9783ac39661b2b1e8f85df73
ylx <18701113138@163.com>
2017-05-16 20:55:37 +0800

    编译原理项目

--- code/main.py
+++ code/main.py
@@ -30,8 +30,45 @@ def get_trace_and_result(num):
     return (trace, result)
 
 
+# 删除偶然正确性测试用例，返回删除后的结果和删除比例
+def delete_accidental_correctness(trace, result, u):
+    u_01 = [1 if tmp > 0.5 else 0 for t in u for tmp in t]
+    # 统计
+    num_correct_c1 = []
+    num_wrong_c1 = []
+    num_correct_c2 = []
+    num_wrong_c2 = []
+    num = len(u)
+    for i in range(num):
+        if u_01[i][0] == 1:
+            if result[i] == 1:
+                num_correct_c1.append(i)
+            else:
+                num_wrong_c1.append(i)
+        else:
+            if result[i] == 1:
+                num_correct_c2.append(i)
+            else:
+                num_wrong_c2.append(i)
+    num_result = [num_correct_c1, num_wrong_c1, num_correct_c2, num_wrong_c2]
+    # 删除
+    del_which = 0 if len(num_result[1]) > len(num_result[3]) else 2
+    trace1 = trace
+    result1 = result
+    for i in num_result[del_which]:
+        trace1.remove(i)
+        result1.remove(i)
+    rate = len(num_result[del_which]) * 1.0 / num
+    return (trace1, result1, rate)
+
+
 # 获取向量信息
-def get_vectors(num):
+def get_vectors(trace, result):
+    num = []
+    len = len(trace)
+    for i in range(len):
+        num.append(trace[i])
+        num[i].append(result[i])
     vectors = num
     vectors = [1 if n > 0 else 0 for vector in vectors for n in vector]
     return vectors
@@ -50,8 +87,17 @@ def init_feature():
 if __name__ == "__main__":
     num = read("a.txt")
     assert (num != None)
-    (trace,result) = get_trace_and_result(num)
+    (trace, result) = get_trace_and_result(num)
     # 偶然正确性
     fcm = FCM
     u = fcm.alg_fcm(trace, fcm.init_u(len(trace), 2), 3, 2, 0.01)
+    # 删除偶然正确性元素
+    (trace1, result1, rate) = delete_accidental_correctness(trace, result, u)
+    print(trace1, result1, rate)
+
+    # 训练
+    # 1.获取向量信息，初始化Feature信息
+    vectors = get_vectors(trace1, result1)
+    features = init_feature()
+
 
